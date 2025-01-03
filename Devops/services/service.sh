3c57090a7d179e654eff1e29dca0a28b6606aee6
Nil <birliknil@gmail.com>
2025-01-03 16:21:29 +0300

    Delete Devops/services directory

--- Devops/services/service.sh
+++
@@ -1,9 +0,0 @@
-docker stack deploy -c postgres.yml postgres
-docker stack deploy -c pgadmin4.yml pgadmin4
-docker stack deploy -c elasticsearch.yml elasticsearch
-docker stack deploy -c kibana.yml kibana
-docker stack deploy -c redis.yml redis
-docker stack deploy -c rabbitmq.yml rabbitmq
-docker stack deploy --with-registry-auth -c dbmigrate.yml migrate
-docker stack deploy --with-registry-auth -c pusula_healthcare.yml pusula
-
--- Devops/services/team1_node2_delete_images.sh
+++
@@ -1,1 +0,0 @@
-sshpass -p '15KkA6n8xbjDjHONtxzR' ssh mehmet-desteci@68.183.208.183 "docker container prune -f && docker image prune -a -f"
